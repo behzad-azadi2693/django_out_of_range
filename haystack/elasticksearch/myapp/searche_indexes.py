816d8f3398aaee45e57b10e9dc51b8815c797cb9
behzad azadi <48598137+behzad-azadi2693@users.noreply.github.com>
2022-08-18 16:45:25 +0430

    Update searche_indexes.py

--- haystack/elasticksearch/myapp/searche_indexes.py
+++ haystack/elasticksearch/myapp/searche_indexes.py
@@ -4,8 +4,8 @@ from haystack import indexes
  
 class CustomerIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.NgramField(document=True, use_template=True)
-    title = indexes.CharField(model_attr='title')
-    descriptions = indexes.CharField(model_attr='descriptions')
+    title = indexes.CharField(model_attr='title', boost=1.1)
+    descriptions = indexes.CharField(model_attr='descriptions', boost=0.8)
 
     class Meta:
         model = Customer
