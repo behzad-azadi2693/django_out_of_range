15e9eab1a458a05a0016e87d7ac17ffe63e01486
behzad azadi <48598137+behzad-azadi2693@users.noreply.github.com>
2022-08-19 19:40:55 +0430

    Update models.py

--- haystack/elasticksearch/myapp/models.py
+++ haystack/elasticksearch/myapp/models.py
@@ -1,12 +1,20 @@
-from pyexpat import model
-from statistics import mode
 from django.db import models
 
 # Create your models here.
-class Customer(models.Model):
-    title = models.CharField(max_length=200)
-    descriptions = models.TextField()
- 
-    def __str__(self) -> str:
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    tag = models.ManyToManyField(Tags)
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    technical_tips = models.TextField()
+
+    def __str__(self):
         return self.title
- 
