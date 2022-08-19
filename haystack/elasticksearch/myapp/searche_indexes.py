f2f3a8bfee306cff3e459163c2d3509a957a0f8c
behzad azadi <48598137+behzad-azadi2693@users.noreply.github.com>
2022-08-19 19:40:28 +0430

    Update searche_indexes.py

--- haystack/elasticksearch/myapp/searche_indexes.py
+++ haystack/elasticksearch/myapp/searche_indexes.py
@@ -1,18 +1,23 @@
-from .models import Customer
+from .models import Post
 from haystack import indexes
  
- 
-class CustomerIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.NgramField(document=True, use_template=True)
-    title = indexes.CharField(model_attr='title', boost=1.1)
-    descriptions = indexes.CharField(model_attr='descriptions', boost=0.8)
 
+class PostIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.NgramField(document=True, use_template=True)
+    title = indexes.CharField(model_attr='title', boost=1)
+    body = indexes.CharField(model_attr='body', boost=1)
+    tag = indexes.MultiValueField(indexed=True, stored=True)
+    technical_tips = indexes.CharField(model_attr='technical_tips')
+    
     class Meta:
-        model = Customer
-        fields = ['text', 'title', 'descriptions']
+        model = Post
+        fields = ['text', 'title', 'body', 'tag', 'technical_tips']
  
     def get_model(self):
-        return Customer
+        return Post
  
     def index_queryset(self, using=None):
-        return self.get_model().objects.all()
+        return self.get_model().objects.all()    
+    
+    def prepare_tag(self, object):
+        return [tag.name for tag in object.tag.all()]
