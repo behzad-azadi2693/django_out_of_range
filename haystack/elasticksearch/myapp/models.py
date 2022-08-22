58478b055cd22ac958a430ec4fc7710c581733fc
behzad azadi <48598137+behzad-azadi2693@users.noreply.github.com>
2022-08-22 16:20:03 +0430

    Update models.py

--- haystack/elasticksearch/myapp/models.py
+++ haystack/elasticksearch/myapp/models.py
@@ -1,4 +1,7 @@
+from distutils.command.upload import upload
 from django.db import models
+from django.utils.text import slugify
+from django.utils.crypto import get_random_string
 
 # Create your models here.
 
@@ -10,11 +13,31 @@ class Tags(models.Model):
         return self.name
 
 
-class Post(models.Model):
+class SeoIdModel(models.Model):
+    seo_id = models.CharField(max_length=255, blank=True)
+    seo_id_field = 'title'
+    
+    def get_seo_id(self):
+        txt = ' '.join(getattr(self,self.seo_id_field).split()[:10])
+        return slugify(txt,True)+'-'+get_random_string(length=8)
+
+    def save(self,*args,**kwargs):
+        if not self.seo_id:
+            self.seo_id = self.get_seo_id()
+        return super().save(*args,**kwargs)
+
+    class Meta:
+        abstract = True
+
+
+class Post(SeoIdModel):
     tag = models.ManyToManyField(Tags)
     title = models.CharField(max_length=250)
     body = models.TextField()
-    technical_tips = models.TextField()
-
+    image = models.ImageField(upload_to = 'post/')
+    is_active = models.BooleanField(default=False)
+    is_remove = models.BooleanField(default=False)
+    
     def __str__(self):
         return self.title
+    
