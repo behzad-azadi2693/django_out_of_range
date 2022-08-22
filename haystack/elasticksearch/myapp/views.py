0304824d1fae6b3d4dd046bb70d83327ddec3418
behzad azadi <48598137+behzad-azadi2693@users.noreply.github.com>
2022-08-22 16:21:56 +0430

    Update views.py

--- haystack/elasticksearch/myapp/views.py
+++ haystack/elasticksearch/myapp/views.py
@@ -6,6 +6,68 @@ from elasticsearch import Elasticsearch
 
 
 class ListSearchSerializer(serializers.ModelSerializer):
+    image_url = serializers.CharField()
+
+    class Meta:
+        model = Post
+        fields = ['id', 'seo_id', 'title', 'body', 'image_url']
+
+
+class ElasticSearchView(ListAPIView):
+    serializer_class = ListSearchSerializer
+    permission_classes = [AllowAny]
+    
+    def get_queryset(self, *args, **kwargs):
+        es= Elasticsearch('http://adminuser:password1234@localhost:9200')
+        query_param = self.request.GET['search_query']
+
+        search = es.search(
+            index = "haystack", 
+            query = {
+                "bool":{
+                    "should":[
+                        {"match":{"title": {"query": query_param,"fuzziness": "AUTO"}}},
+                        {"match":{"body": {"query": query_param,"fuzziness": "AUTO"}}},
+                        {"match":{"seo_id": {"query": query_param,"fuzziness": "AUTO"}}},
+                        {"match":{"tag": {"query": query_param,"fuzziness": "AUTO"}}},
+                    ],
+                    "filter":[
+                        {"term":{"is_active":"true"}},
+                        {"term":{"is_remove":"false"}},
+                    ],
+                    "minimum_should_match":1,
+                    "boost":1.0,
+                },
+            }
+        )
+        
+        response = [
+                {
+                    "id":hit["_source"]["id"].split('.')[-1],
+                    "seo_id":hit["_source"]["seo_id"],
+                    "title":hit["_source"]["title"],
+                    "body":hit["_source"]["body"],
+                    "image_url":hit["_source"]["image_url"],
+                } 
+                for hit in search['hits']['hits']
+            ]
+ 
+        return response
+ 
+
+
+
+
+
+'''
+from rest_framework.generics import ListAPIView
+from rest_framework import serializers
+from .models import Post
+from rest_framework.permissions import AllowAny
+from elasticsearch import Elasticsearch
+
+
+class ListSearchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id','title', 'body', 'technical_tips']
@@ -41,4 +103,4 @@ class ElasticSearch(ListAPIView):
  
         return response
  
-
+'''
