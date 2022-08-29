2fe3eda77cb478694efa2395652039a766a05da9
behzad azadi <48598137+behzad-azadi2693@users.noreply.github.com>
2022-08-29 14:46:58 +0430

    Update serializers.py

--- haystack/elasticksearch/myapp/serializers.py
+++ haystack/elasticksearch/myapp/serializers.py
@@ -1,6 +1,10 @@
+from rest_framework import serializers
+
+
 class ListSearchSerializer(serializers.ModelSerializer):
     image_url = serializers.CharField()
+    tags = serializers.ListField()
 
     class Meta:
         model = Post
-        fields = ['id', 'seo_id', 'title', 'body', 'image_url']
+        fields = ['id', 'seo_id', 'title', 'tags', 'body', 'image_url']
