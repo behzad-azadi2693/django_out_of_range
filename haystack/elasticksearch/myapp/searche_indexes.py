c6505e2c905e58b7cf43f4c024184a0062a703a3
behzad azadi <48598137+behzad-azadi2693@users.noreply.github.com>
2022-08-29 14:47:26 +0430

    Update searche_indexes.py

--- haystack/elasticksearch/myapp/searche_indexes.py
+++ haystack/elasticksearch/myapp/searche_indexes.py
@@ -7,14 +7,14 @@ class PostIndex(indexes.SearchIndex, indexes.Indexable):
     seo_id = indexes.CharField(model_attr='seo_id')
     title = indexes.CharField(model_attr='title', boost=1)
     body = indexes.CharField(model_attr='body', boost=1)
-    tag = indexes.MultiValueField(indexed=True, stored=True)
+    tags = indexes.MultiValueField(indexed=True, stored=True)
     image_url = indexes.CharField()
     is_active = indexes.BooleanField(model_attr='is_active')
     is_remove = indexes.BooleanField(model_attr='is_remove')
 
     class Meta:
         model = Post
-        fields = ['text', 'title', 'body', 'tag', 'seo_id', 'image_url', 'is_active', 'is_remove']
+        fields = ['text', 'title', 'body', 'tags', 'seo_id', 'image_url', 'is_active', 'is_remove']
  
     def get_model(self):
         return Post
@@ -22,8 +22,8 @@ class PostIndex(indexes.SearchIndex, indexes.Indexable):
     def index_queryset(self, using=None):
         return self.get_model().objects.all()    
     
-    def prepare_tag(self, object):
-        return [tag.name for tag in object.tag.all()]
+    def prepare_tags(self, object):
+        return [tag.name for tag in object.tags.all()]
 
     def prepare_image_url(self, object):
         return object.image.path
